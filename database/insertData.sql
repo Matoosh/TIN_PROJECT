0e3e56b16af3b23706b6ccc6ac3bcf0c614a820e
Mateusz Niełacny <19360197+Matoosh@users.noreply.github.com>
2021-01-11 13:15:06 +0100

    add insert query with example data to database

---
+++ database/insertData.sql
@@ -0,0 +1,76 @@
+INSERT INTO author (first_name, last_name, birth, imglink) VALUES
+                                                            ('John Ronald Reuel','Tolkien','1892-01-03', null),
+                                                            ('Cora','Reilly','1990-01-01', null),
+                                                            ('Remigiusz','Mróz','1987-01-15', null),
+                                                            ('Bernadette Anne','MacDoughall','1958-01-01', null),
+                                                            ('Alicja','Sinicka','1987-04-15', null),
+                                                            ('Beth','Reekles','1995-06-07', null);
+
+INSERT INTO account (login, password, email, registerdate, role) VALUES
+                                                            ('admin', 'admin', 'admin@goodreads.com', now(), 'admin'),
+                                                            ('user', 'user', 'user@goodreads.com', now(), 'user'),
+                                                            ('test', 'test', 'test@goodreads.com', now(), 'user')
+
+
+INSERT INTO book (title, description, releasedate, status, imglink, author_id) values
+('Złączeni przeszłością','Moje życie to historia zdrady i kłamstwa
+Zabiłem tak wielu ludzi, ponieważ zaszkodzili naszej sprawie, ponieważ zdradzili nasz Oddział.
+Hipokryta. Kłamca. Morderca.
+Tym właśnie jestem.
+Zabiłem tak wielu, a sam zdradziłem Oddział pięć razy. Przysięgałem, ręczyłem własną krwią i życiem, że mafia będzie zawsze na pierwszym miejscu.
+Pięć razy to kobieta była dla mnie na pierwszym miejscu. Zdradziłem mojego ojca, złamałem przysięgę, zdradziłem moich ludzi.
+Czy moje zdrady zniszczą w końcu wszystko, co przysięgałem chronić?
+
+~Valentina
+W dniu naszego ślubu złożyłam przysięgę, że zawsze będę stała po stronie Dantego.
+Na dobre i na złe.
+Że będę go kochała bez względu na wszystko.
+Dorastając w mafii, wiedziałam, co nas czeka. Że stojące przed nami wyzwania, nie będą miały końca. Nigdy jednak nie sądziłam, że sprawy zajdą aż tak daleko. ',null, null, null, 1),
+('Słodka Pokusa','Po stracie żony, Cassio zostaje sam z dwójką małych dzieci. Musi szybko znaleźć dla nich matkę, a dla siebie kobietę, która ogrzeje jego łóżko. Jest mężczyzną, który ma władzę, a za władzą podążają obowiązki.
+Mafioso doskonale wie, że w jego świecie wybór żony to nakaz a nie przyjemność. Nie ma nic ważniejszego niż tradycja i honor. I w taki oto sposób jego żoną zostaje ledwo pełnoletnia dziewczyna.
+Giulia zawsze wiedziała, że poślubi mężczyznę, którego wybierze dla niej ojciec. Ale nigdy by nie przypuszczała, że będzie on dużo starszy od niej. Nagle ma zostać matką dla dwójki małych dzieci, kiedy nigdy nie trzymała nawet dziecka w ramionach. Giulia szybko się orientuje, że Cassio nie jest zainteresowany żadnym prawdziwym związkiem z nią. Szukał jedynie niani dla swoich dzieci. Jednak dziewczyna postanawia zawalczyć o swoją wizję szczęścia. ',null, null, null, 1),
+('Złamana lojalność','Fabiano od najmłodszych lat był wychowywany z myślą o tym, że pójdzie w ślady ojca i podobnie jak on zostanie consigliere w chicagowskim oddziale. Jednak tak się nie dzieje. Skazany na śmierć, ledwo żywy czternastoletni chłopak walczy o przetrwanie.
+
+Mafijny świat jest jedynym, jaki zna, więc kiedy Fabiano spotyka Remo Falcome, stawia sobie nowy cel: stać się częścią innej rodziny mafijnej. Ale żeby tak się stało, musi na to zasłużyć.
+
+I zasłuży – walcząc w klatkach i zyskując miano mężczyzny, którego trzeba się bać. W ten sposób delikatny Fabi odchodzi na zawsze. Jego miejsce zajmuje bezwzględny i bardzo niebezpieczny człowiek.
+
+Leona pragnie zwyczajnego życia, z dala od matki narkomanki. Jednak gdy kobieta wpada w oko Fabiano Scuderiemu, jej marzenie pryska jak bańka mydlana.
+
+Dla Fabiano liczy się tylko Camorra. Czy będzie gotowy zaryzykować wszystko, o co tak ciężko walczył? ',null, null, null, 1),
+('Bound by Blood','Born in Blood
+Sworn in Blood…
+…isn’t only the motto the Famiglia lives by.
+Bleeding for love is something every couple in this book has experienced. Through hardships their love morphs into something even more beautiful and resilient. A love each of them is willing to fight for.
+
+This anthology contains stories for the following couples:
+
+Aria & Luca
+Romero & Liliana
+Growl & Cara
+Mauro & Stella
+Matteo & Gianna ',null, null, null, 1),
+('Złączeni miłością','Nikt nie przypuszczał, że się w sobie zakochają.
+Kiedy Aria została oddana Luce w akcie małżeństwa, wszyscy byli pewni, że przyszły Capo ją złamie.
+Aria była tego pewna.
+Mężczyzna taki jak on, nieznający litości, nieczujący nic prócz nienawiści.
+Jednak to, co niemożliwe, zdarzyło się. Zakochali się w sobie.
+Ale Luca wie, zawsze wiedział, że miłość w jego świecie, na jego pozycji to ryzyko.
+Kiedy Aria zdradza Lukę, próbując chronić swoją rodzinę, szybko się orientuje, że może stracić coś, o co tak długo walczyła. Zaufanie Luki.
+Zaufanie ze strony mężczyzny, który nigdy nie zaufał nikomu bezgranicznie. Czy teraz zrozumie, że nigdy nie powinien? ',null, null, null, 1),
+('Fragile Longing','Sofia knows how it feels to be the consolation prize.
+Too young.
+Not blond.
+And definitely not an ice princess.
+Her sister is—was all those things. Perfection. Until she wasn’t. Until she ran off to be with the enemy and left her fiancé behind.
+Now Sofia is given to Danilo in her sister’s stead, knowing she’ll never be more than second best. Yet, she can’t stop longing for the love of the man she’s been crushing on even when he was still her sister’s.
+Danilo is a man who’s used to get what he wants.
+Power.
+Respect.
+The sought-after ice princess.
+Until another man steals his bride-to-be. Danilo knows that for a man in his position losing his woman can lead to a loss of face.
+Wounded pride.
+Thirst for revenge.
+A dangerous combination—one Danilo can’t leave behind, not even when a girl just as precious takes her sister’s place to placate him. Yet, she’s got one flaw: she’s not her sister.
+Unable to forget what he’s lost, Danilo might lose what he’s been given. ',null, null, null, 1)
+
