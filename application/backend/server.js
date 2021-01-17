80d5ca5fe4af9d0de6506b91a0e2721ec94106c4
Mateusz Niełacny <19360197+Matoosh@users.noreply.github.com>
2021-01-17 21:06:35 +0100

    Deleted sync() from sequelize

--- application/backend/server.js
+++ application/backend/server.js
@@ -11,9 +11,16 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const db = require("./app/models");
-db.sequelize.sync();
+db.sequelize;
 
 if(!db)
     console.log("Error connecting db")
@@ -25,6 +32,7 @@ require("./app/routes/account.routes")(app);
 require("./app/routes/book.routes")(app);
 require("./app/routes/comment.routes")(app);
 require("./app/routes/auth.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => {
